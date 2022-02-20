910fb3c073ba80b8ce9a51fa49894726ddf3e12f
dedrie p <dedrie@dedries-MacBook-Air.local>
2022-02-20 04:04:14 -0500

    fixed questions function and markdown function

--- Develop/index.js
+++ Develop/index.js
@@ -3,13 +3,15 @@
 const generateMarkdown = require("./utils/generateMarkdown");
 const fs = require('fs')
 var inquirer = require('inquirer')
+//var title = questions[0];
 
 
 console.log("hello world")
 // TODO: Create an array of questions for user input
 
-const questions = inquirer.prompt = () => {
-    
+const questions = () => {
+
+    inquirer.prompt (
     [
         {
         type: 'input',
@@ -37,7 +39,7 @@ const questions = inquirer.prompt = () => {
 
     {
         type: 'input',
-        name: 'contricution',
+        name: 'contribution',
         message: 'Provide contribution information'
 
 
@@ -45,21 +47,21 @@ const questions = inquirer.prompt = () => {
 
     {
         type: 'input',
-        name: 'title',
-        message: 'What is your project title'
+        name: 'Tests',
+        message: 'Provide test information'
 
 
     },
-]
-return inquirer.prompt()
+]) .then(answers => console.log(answers));
+
 };
 
-questions().then(answers => console.log(answers));
+questions()
 
 
 // TODO: Create a function to write README file
 //function writeToFile(readMe, data) {}
-// fs.writeFile('readMe.md', generateMarkdown(data), err => {
+//fs.writeFile('./readMe.md', generateMarkdown(data), err => {
 //     if (err) throw err;
   
 //     console.log('Portfolio complete! Check out index.html to see the output!');
