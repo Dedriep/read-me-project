9ea4c48497d8baf95f7be566389ef5b2caf6e109
dedrie p <dedrie@dedries-MacBook-Air.local>
2022-02-25 03:18:40 -0500

    cleaned up function

--- Develop/readMe.md
+++ Develop/readMe.md
@@ -1,9 +1,18 @@
-# <undefined>
-  ## description: undefined;
-  ## INSTALLATION: undefined;
-  ## CONTRIBUTION: undefined;
-  ## TESTS: undefined
-  ## Questions undefined undefined;
-  
+# <d>
+function renderLicenseBadge(license) {
+
+  if (!license) { 
+    return '' 
+}else {
+(license === license.MIT) 
+    return ` https://img.shields.io/badge/license-MIT-brightgreen`
+}
+}
+  ## description: f;
+  ## INSTALLATION: ;
+  ## CONTRIBUTION: h;
+  ## TESTS: j
+  ## Questions rtag r;
+  ## LICENSE: This project is protected under: MIT 
 
 
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -2,11 +2,19 @@
 
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+
+  if (!license) { 
+    return '' 
+}else {
+(license === license.MIT) 
+    return ` https://img.shields.io/badge/license-MIT-brightgreen`
+}
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -14,24 +22,29 @@ function renderLicenseSection(license) {
 
   if (!license) {
     return ''
-  
-  } else { 
-    return `
-    ##This project is protected under: ${license} `
+
+  } else {
+    return `## LICENSE: This project is protected under a ${license} license.`
   }
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(answers) {
-  const {title, description, installation, contribution, tests, license, ...aboutme} = generateMarkdown
-console.log(title)
-console.log("from generate markdown")
-console.log(answers)
-return `# <${title}>
-  ## description: ${description};
-  ## INSTALLATION: ${installation};
-  ## CONTRIBUTION: ${contribution};
-  ## TESTS: ${tests}
+  const { title, description, installation, contribution, tests, license, ...aboutme } = answers
+
+  console.log(title)
+  console.log("from generate markdown")
+  console.log(answers)
+  return `# <${title}>
+${renderLicenseBadge}
+  ## description: 
+  ${description};
+  ## INSTALLATION: 
+  ${installation};
+  ## CONTRIBUTION: 
+  ${contribution};
+  ## TESTS: 
+  ${tests}
   ## Questions ${aboutme.email} ${aboutme.username};
   ${renderLicenseSection(license)}
 
